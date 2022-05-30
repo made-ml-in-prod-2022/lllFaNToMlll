29a964d3192eb60283ec45a62fef84e0b3207e82
lllFaNToMlll <iborisik30@gmail.com>
2022-05-30 14:36:40 +0300

    add HW2

--- online_inference/README.md
+++ online_inference/README.md
@@ -2,44 +2,59 @@
 
 online inference (работа с docker и FastAPI)
 
-#### Запуск тестов из директории online inference
+#### Репозиторий в hub.docker
 
+https://hub.docker.com/repository/docker/lllfantomlll/lllfantomlll/general
+
+#### Собрать контейнер из директории online inference
 ~~~
-python -m pytest tests\tests.py
+docker build -t lllfantomlll:ready .
 ~~~
 
-#### Загрузка образа из ретозитория hub.docker
+#### Загрузка образа из ретозитория hub.docker из директории online inference
 ~~~
 docker pull lllfantomlll/lllfantomlll:ready
 ~~~
 
-#### Запуск контейнера с docker образом
+#### Запуск контейнера с docker образом из директории online inference
 ~~~
+В одном окне консоли:
 docker run --rm -p 80:80 lllfantomlll/lllfantomlll:ready
+В другом окне консоли:
+python make_predict_request.py
 ~~~
+
+#### Запуск приложения из директории online inference
+~~~
+В одном окне консоли:
+python app.py
+В другом окне консоли:
+python make_predict_request.py
+~~~
+
 
 Организация проекта
 ------------
     ├── data
     │   ├── raw.csv             <- Реальные данные.
-	│	└── test_data.csv       <- Данные для теста.
-	│
+    │   └── test_data.csv       <- Данные для теста.
+    │
     ├── models                  <- Сохраненные модели, трансформеры.
     │
-	├── source              	<- Папка с классами для данных.
-	│
-	├── tests                   <- Тесты.
-	│
-	├── app.py                  <- Реализация общения с моделью через FastAPI.
+    ├── source                  <- Папка с классами для данных.
+    │
+    ├── tests                   <- Тесты.
+    │
+    ├── app.py                  <- Реализация общения с моделью через FastAPI.
     │
     ├── Dockerfile              <- Файл с настройками для docker.
-	│
-	├── envinit.sh              <- Файл с настройками для виртуального окружения.
-	│
-	├── make_predict_request	<- Скрипт для отправки команды предсказания.
-	│
-	├── README.md               <- Инструкция к проекту.
-	│
+    │
+    ├── envinit.sh              <- Файл с настройками для виртуального окружения.
+    │
+    ├── make_predict_request    <- Скрипт для отправки команды предсказания.
+    │
+    ├── README.md               <- Инструкция к проекту.
+    │
     ├── requirements.txt        <- Необходимые библиотеки для работы проекта.
 
 --------
--- online_inference/tests/tests.py
+++ online_inference/tests/tests.py
@@ -1,52 +1,55 @@
+import unittest
 from fastapi.testclient import TestClient
 import pandas as pd
 import numpy as np
 from online_inference.app import app
 
 
-def test_start():
-    """Проверка функции get"""
-    with TestClient(app) as client:
-        response = client.get('/')
-        assert response.status_code == 200
+class MyTestCase(unittest.TestCase):
+    def test_start(self):
+        """Проверка функции get"""
+        with TestClient(app) as client:
+            response = client.get('/')
+            self.assertEqual(response.status_code, 200)
+
+    def test_health(self):
+        """Проверка функции health"""
+        with TestClient(app) as client:
+            response = client.get('/health')
+            self.assertEqual(response.status_code, 200)
+            self.assertTrue(response.json())
+
+    def test_predict(self):
+        """Проверка работы функции предсказания"""
+        real_response = [
+            [{'id': 0, 'target': 0}],
+            [{'id': 0, 'target': 1}],
+        ]
+
+        with TestClient(app) as client:
+            data = pd.read_csv('../data/test_data.csv')
+            request_features = list(data.columns)
+            for i, _ in enumerate(data.shape):
+                request_data = [
+                    x.item() if isinstance(x, np.generic) else x for x in data.iloc[i].tolist()
+                ]
+
+                response = client.get(
+                    '/predict',
+                    json={
+                        'data': [request_data],
+                        'features': request_features,
+                    },
+                )
+                self.assertEqual(response.status_code, 200)
+                self.assertEqual(response.json(), real_response[i])
+
+    def test_bad_command(self):
+        """Проверка на корректную обработку неправильной команды"""
+        with TestClient(app) as client:
+            response = client.get('/badcommand')
+            self.assertEqual(response.status_code, 404)
 
 
-def test_health():
-    """Проверка функции health"""
-    with TestClient(app) as client:
-        response = client.get('/health')
-        assert response.status_code == 200
-        assert response.json() is True
-
-
-def test_predict():
-    """Проверка работы функции предсказания"""
-    real_response = [
-        [{'id': 0, 'target': 0}],
-        [{'id': 0, 'target': 1}],
-    ]
-
-    with TestClient(app) as client:
-        data = pd.read_csv('data/test_data.csv')
-        request_features = list(data.columns)
-        for i, _ in enumerate(data.shape):
-            request_data = [
-                x.item() if isinstance(x, np.generic) else x for x in data.iloc[i].tolist()
-            ]
-
-            response = client.get(
-                '/predict',
-                json={
-                    'data': [request_data],
-                    'features': request_features,
-                },
-            )
-            assert response.status_code == 200
-            assert response.json() == real_response[i]
-
-
-def test_bad_command():
-    """Проверка на корректную обработку неправильной команды"""
-    with TestClient(app) as client:
-        response = client.get('/badcommand')
-        assert response.status_code == 404
+if __name__ == '__main__':
+    unittest.main()
